f96fc020b8dcab0482adf42a610992322226188b
Nelson Moreno <thegreyabove@gmail.com>
2022-04-06 16:17:32 -0400

    5th commit

--- public/main.js
+++ public/main.js
@@ -1,1 +1,7 @@
 console.log('JS Connected')
+var hotItems = document.querySelectorAll('li.hot');
+if(hotItems.length > 0 ) {
+    for(let i = 0; i <hotItems.length; i++) {
+        hotItems[i].className = 'cool';
+    }
+}
